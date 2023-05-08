c8b4e23265921ccd411d98356897f329ba87cb11
kyocoro <kamibayashikiri@gmail.com>
2023-05-09 02:04:43 +0900

    Rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
-    password_confirmation {password}
-    last_name             {Gimei.last.kanji}
-    first_name            {Gimei.first.kanji}
-    last_name_k           {Gimei.last.katakana}
-    first_name_k          {Gimei.first.katakana}
-    birthday              {Faker::Date.between(from: '1990-01-01', to:'2020-12-08')}
+    nickname              { 'test' }
+    email                 { 'test@example' }
+    password              { '000000' }
+    password_confirmation { password }
+    last_name             { Gimei.last.kanji }
+    first_name            { Gimei.first.kanji }
+    last_name_k           { Gimei.last.katakana }
+    first_name_k          { Gimei.first.katakana }
+    birthday              { Faker::Date.between(from: '1990-01-01', to: '2020-12-08') }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,47 +7,47 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it "nicknameとemail、passwordとpassword_confirmation、last_name、first_name、last_name_k、first_name_k、birthdayが存在すれば登録できる" do
+      it 'nicknameとemail、passwordとpassword_confirmation、last_name、first_name、last_name_k、first_name_k、birthdayが存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
     context '新規登録できない場合' do
-      it "nicknameが空では登録できない" do
+      it 'nicknameが空では登録できない' do
         @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
-      it "emailが空では登録できない" do
+      it 'emailが空では登録できない' do
         @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
-      it "passwordが空では登録できない" do
+      it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-      it "last_nameが空では登録できない" do
+      it 'last_nameが空では登録できない' do
         @user.last_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last_name can't be blank")
       end
-      it "first_nameが空では登録できない" do
+      it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First_name can't be blank")
       end
-      it "last_name_kが空では登録できない" do
+      it 'last_name_kが空では登録できない' do
         @user.last_name_k = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last_name_k can't be blank")
       end
-      it "first_name_kが空では登録できない" do
+      it 'first_name_kが空では登録できない' do
         @user.first_name_k = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First_name_k can't be blank")
       end
-      it "birthdayが空では登録できない" do
+      it 'birthdayが空では登録できない' do
         @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
@@ -60,8 +60,8 @@ RSpec.describe User, type: :model do
       end
 
       it 'passwordが129文字以上では登録できない' do
-        @user.password =  Faker::Internet.password(min_length: 129)
-        @user.password_confirmation =  @user.password
+        @user.password = Faker::Internet.password(min_length: 129)
+        @user.password_confirmation = @user.password
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
       end
@@ -79,7 +79,7 @@ RSpec.describe User, type: :model do
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
 
-     it 'emailは@を含まないと登録できない' do
+      it 'emailは@を含まないと登録できない' do
         @user.email = 'testmail'
         @user.valid?
         expect(@user.errors.full_messages).to include('Email is invalid')
