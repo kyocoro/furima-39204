1c3cdcb6aad5954d2029323c82c24501ca83adab
kyocoro <kamibayashikiri@gmail.com>
2023-05-09 17:12:03 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,14 @@ class User < ApplicationRecord
   # has_many :comments
   # has_many :histories
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_k, presence: true
-  validates :first_name_k, presence: true
-  validates :birthday, presence: true
+    validates :nickname, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります’}
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name, presence: true
+    validates :first_name, presence: true
+    validates :last_name_k, presence: true
+    validates :first_name_k, presence: true
+  end
+    validates :birthday, presence: true
 end
