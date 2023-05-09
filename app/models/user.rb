0912232e4f5ff041d12a0e4befa371852d3d1efc
kyocoro <kamibayashikiri@gmail.com>
2023-05-09 18:05:38 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name, presence: true
     validates :first_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' } do
     validates :last_name_k, presence: true
     validates :first_name_k, presence: true
   end
