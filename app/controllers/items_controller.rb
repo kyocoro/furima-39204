ebde118f899e664c104e055c28d9a1c2e3dc5efa
kyocoro <kamibayashikiri@gmail.com>
2023-05-11 20:21:21 +0900

    ログイン状態のユーザーだけが、商品出品ページへ遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+   # @items = Item.all
   end
 
   def new
@@ -19,5 +19,9 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :charge_id, :region_id, :period_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
