428603eab15fc2f20b03779f2acf97df45bdecaa
kyocoro <kamibayashikiri@gmail.com>
2023-05-11 20:40:37 +0900

    金額の範囲のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,5 +22,6 @@ class Item < ApplicationRecord
   validates :charge_id, presence: true
   validates :region_id, presence: true
   validates :period_id, presence: true
-  validates :price, presence: true
+  with_options presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 } do
+    validates :price
 end
