1c65096021d5d770729aa52ac482a88bf2e452b5
kyocoro <kamibayashikiri@gmail.com>
2023-05-11 20:47:15 +0900

    テスト追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -57,6 +57,17 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
+      it "価格が299円以下だと出品できない" do
+        @item.price = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        end
+        it "価格が10000000円以上だと出品できない" do
+        @item.price = 10000001
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        end
+        
     end
   end
 end
