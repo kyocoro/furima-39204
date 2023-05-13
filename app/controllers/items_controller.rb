4d837e3a4eb093a8703d3dc0ff4e02106ce2a3a8
kyocoro <kamibayashikiri@gmail.com>
2023-05-13 17:31:15 +0900

    投稿者本人のみ編集・削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,7 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :charge_id, :region_id, :period_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:img-content, :image).merge(user_id: current_user.id)
   end
 
 end
