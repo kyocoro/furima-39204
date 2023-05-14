09c57124150d9978c7acaadf461d64cd4cfc8f36
kyocoro <kamibayashikiri@gmail.com>
2023-05-14 16:36:20 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-   # @items = Item.all
+    @items = Item.all.order(created_at: :desc)
   end
 
   def new
@@ -17,10 +17,26 @@ class ItemsController < ApplicationController
     end
   end
 
-  
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
   private
   def item_params
-    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :charge_id, :region_id, :period_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :charge_id, :region_id, :period_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
