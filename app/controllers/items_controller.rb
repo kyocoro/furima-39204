06f832e133d7a17e3bbc5b47167e4e4c6e6c302d
kyocoro <kamibayashikiri@gmail.com>
2023-05-15 16:17:04 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -18,25 +21,39 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def set_item
     @item = Item.find(params[:id])
   end
 
-  #def edit
-    #@item = Item.find(params[:id])
-  #end
-
-  #def destroy
-    #@item = Item.find(params[:id])
-    #if @item.destroy
-      #redirect_to root_path
-    #else
-      #redirect_to root_path
-    #end
-  #end
-
-  private
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :charge_id, :region_id, :period_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user == @item.user
   end
 
 end
