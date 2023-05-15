d5697b504a76761507490f99fb4412d650701408
kyocoro <kamibayashikiri@gmail.com>
2023-05-15 16:00:05 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
   before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :contributor_confirmation, only: [:edit, :update]
   
@@ -20,14 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
+    
   end
 
   def update
-    
+  
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -45,6 +47,10 @@ class ItemsController < ApplicationController
   #end
 
   private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :charge_id, :region_id, :period_id, :price, :image).merge(user_id: current_user.id)
   end
@@ -53,4 +59,5 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     redirect_to root_path unless current_user == @item.user
   end
+
 end
