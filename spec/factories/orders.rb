609a504e43485f12f0bc980be11e9b664f4c8521
kyocoro <kamibayashikiri@gmail.com>
2023-05-20 15:59:19 +0900

    カード情報削除

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,8 +1,5 @@
 FactoryBot.define do
   factory :order do
-    cardnumber {4242424242424242}
-    expiration {"12/25"}
-    securitycode{123}
     token {"tok_abcdefghijk00000000000000000"}
   end
 end
