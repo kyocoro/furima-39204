bbd536a3a94ca795428ad8c1eea56fc13c86dbff
kyocoro <kamibayashikiri@gmail.com>
2023-05-20 15:50:57 +0900

    バリデーション追加

--- app/models/order_purchase.rb
+++ app/models/order_purchase.rb
@@ -8,11 +8,14 @@ class OrderPurchase
     validates :municipalities
     validates :address
     validates :telephone, format: { with: /\A\d{10,11}\z/ }
+    validates :user_id
+    validates :item_id
+    validates :token
   end
 
   #ジャンルの選択が「---」の時は保存できないようにする
   validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
-  
+
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Purchase.create(postcode: postcode, region_id: region_id, municipalities: municipalities, address: address, building_name: building_name, telephone: telephone, order_id: order.id)
